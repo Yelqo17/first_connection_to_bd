f46cf20e43f19ce2e5dff5a7832465d5c0900dee
Sirius Black <an.kozimirov@gmail.com>
2023-10-31 18:49:07 +0300

    add 1 version

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        MyConnect myConnect = new MyConnect();
+        String select = """
+                select *
+                from audit_department
+                """;
+        myConnect.executeSelect(select);
+    }
+}
---
+++ src/MyConnect.java
@@ -0,0 +1,38 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+public class MyConnect {
+    private final String url;
+    private final String user;
+    private final String password;
+    public MyConnect() {
+        url = "jdbc:postgresql://localhost:5432/postgres";
+        user = "postgres";
+        password = "pig51423";
+    }
+    public void executeSelect(String select){
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(select);
+            System.out.printf("%-20s%-20s%-20s\n", "Ревизионный номер", "Тип изделия", "Адрес расположения");
+
+            while (resultSet.next()) {
+                String num = resultSet.getString("Ревизионный номер");
+                String type = resultSet.getString("Тип изделия");
+                String address = resultSet.getString("Адрес расположения");
+
+                System.out.printf("%-20s%-20s%-20s\n", num, type, address);
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException ex) {
+            System.out.println("Cannot connect to DB: " + ex.getMessage());
+        }
+    }
+}
